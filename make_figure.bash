7555005a02537fb05d8d376226f6ce180d353f5f
Adam Schneider <adam.schneider@noaa.gov>
2025-12-19 13:06:36 -0700

    update NNJA plotting script

--- make_figure.bash
+++ make_figure.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python src/score_plotting/core_scripts/plot_gsi_radiance_fit_to_obs_rod_gdas_scout_cfsr.py --days_to_smooth 30 --satellite 'NOAA 15' --channel 7 --rmse_legend --yaxis_obs_err_scaler 0.6
+python src/score_plotting/core_scripts/plot_gsi_radiance_fit_to_obs_rod_gdas_cfsr.py --days_to_smooth 30 --satellite 'NOAA 15' --channel 7 --rmse_legend --yaxis_obs_err_scaler 0.6
