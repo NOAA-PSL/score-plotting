9f6e1355dcb05ceb69dff9e9a1973a5d36186319
Adam Schneider <adam.schneider@noaa.gov>
2026-07-15 16:10:17 -0600

    add plotting generation scripts for 2013 and 2018 scout run streams (v0.2)

---
+++ src/score_plotting/core_scripts/plot_gsi_radiance_fit_to_obs_daily_update_scout_v2_2013.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+""" wrapper to call plot_gsi_radiance_fit_to_obs figure
+generation for a subset of sensors for the replay overlap experiment
+"""
+
+from score_plotting.core_scripts import plot_gsi_radiance_fit_to_obs
+
+def main():
+    plot_gsi_radiance_fit_to_obs.prun(
+        sensor_list = [
+            # microwave sounders:
+            'amsua',
+            'amsub',
+            'atms',
+            'ssmi',
+            'ssmis',
+            # infrared sounders:
+            'airs',
+            # TIROS operational vertical sounders (TOVS):
+            'hirs2',
+            'hirs3',
+            'hirs4',
+            'ssu',
+            'msu',
+            # Advanced Very-High-Resolution Radiometers:
+            'avhrr2',
+            'avhrr3',
+            # others
+            'gmi',
+            'mhs',
+            'saphir',
+            'sndr',
+            'sndrD1',
+            'sndrD2',
+            'sndrD3',
+            'sndrD4',
+            'amsre',
+            'amrs2',
+            'tmi'
+        ],
+        start_date='2012-10-01 00:00:00',
+        stop_date='2014-03-30 23:59:59'
+    )
+
+if __name__=='__main__':
+    main()
---
+++ src/score_plotting/core_scripts/plot_gsi_radiance_fit_to_obs_scout_v2_2018.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+""" wrapper to call plot_gsi_radiance_fit_to_obs figure
+generation for a subset of sensors for the replay overlap experiment
+"""
+
+from score_plotting.core_scripts import plot_gsi_radiance_fit_to_obs
+
+def main():
+    plot_gsi_radiance_fit_to_obs.prun(
+        start_date='2017-10-01 00:00:00',
+        stop_date='2019-03-01 23:59:59'
+    )
+
+if __name__=='__main__':
+    main()
