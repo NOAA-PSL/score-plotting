79a624aaab6eba2e0f0ec8d4ef7ad40ea254708a
Adam Schneider <adam.schneider@noaa.gov>
2026-08-07 17:52:19 -0600

    extend 1997 monitoring plots through Sep 1999

--- src/score_plotting/core_scripts/plot_gsi_conv_fit_to_obs_scout_v2_1997.py
+++ src/score_plotting/core_scripts/plot_gsi_conv_fit_to_obs_scout_v2_1997.py
@@ -7,7 +7,7 @@ generation for a subset of sensors for the replay overlap experiment
 from score_plotting.core_scripts import plot_gsi_conv_fit_to_obs_scout_v2
 
 def main():
-    plot_gsi_conv_fit_to_obs_scout_v2.run(start_date='1996-10-01 00:00:00', stop_date='1999-03-30 23:59:59')
+    plot_gsi_conv_fit_to_obs_scout_v2.run(start_date='1996-10-01 00:00:00', stop_date='1999-10-01 00:00:00')
 
 if __name__=='__main__':
     main()
--- src/score_plotting/core_scripts/plot_gsi_radiance_fit_to_obs_scout_v2.py
+++ src/score_plotting/core_scripts/plot_gsi_radiance_fit_to_obs_scout_v2.py
@@ -9,7 +9,7 @@ from score_plotting.core_scripts import plot_gsi_radiance_fit_to_obs
 def main():
     plot_gsi_radiance_fit_to_obs.prun(
         start_date='1996-10-01 00:00:00',
-        stop_date='1999-03-01 23:59:59'
+        stop_date='1999-10-01 00:00:00'
     )
 
 if __name__=='__main__':
